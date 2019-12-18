630cf5b8f6e1e3b05dadc8df80cab2061ecaafd8
Amay Kataria <amkatari@microsoft.com>
2019-12-18 01:05:27 -0600

    Seperate out the two agents

---
+++ src/AgentA.js
@@ -0,0 +1,69 @@
+class AgentA {
+    constructor() {
+        // Load all the elements
+        this.getHTMLElements(); 
+    }
+
+    getHTMLElements() {
+        this.voicesSelect = document.getElementById("voicesA");
+        this.voicesSelect.addEventListener('change', this.onVoiceSelected.bind(this));
+
+        this.language = document.getElementById("langA");
+        //Pitch
+        this.pitchSlider = document.getElementById('pitchA');
+        this.pitchSlider.addEventListener('input', this.onPitchChange.bind(this)); 
+        // Rate
+        this.rateSlider = document.getElementById('rateA'); 
+        this.rateSlider.addEventListener('input', this.onRateChange.bind(this)); 
+        this.pitchVal = document.getElementById('pitchvalA'); 
+        this.rateVal = document.getElementById('ratevalA');
+        this.emotion = document.getElementById("emotionA"); 
+        this.response = document.getElementById("responseA");
+    }
+
+    setVoices(voices) {
+        for (var i = 0; i < voices.length; i++) {
+            var name = voices[i].name; 
+            var lang = voices[i].language; 
+            var option = document.createElement("option");
+            option.text = name; 
+            option.setAttribute('lang', lang);
+            this.voicesSelect.add(option);
+        }
+
+        // Assign the voice and language
+        this.voicesSelect.value = voices[0].name;
+        this.language.innerText = voices[0].language;
+
+        this.pitchVal.innerText = this.getPitch(); this.rateVal.innerText = this.getRate(); 
+    }
+    
+    onVoiceSelected(event) {
+        var targetEl = event.target; 
+      
+        // Get name and language. 
+        var idx = targetEl.selectedIndex; 
+        var lang = targetEl[idx].lang; 
+      
+        // Set name and language. 
+        this.language.innerText = lang;
+    }
+
+    onPitchChange() {
+        // Set default pitch and rate. 
+        this.pitchVal.innerText = this.getPitch();
+    }
+      
+    onRateChange() {
+        // Set default pitch and rate. 
+        this.rateVal.innerText = this.getRate(); 
+    }
+      
+    getPitch() {
+        return this.pitchSlider.value/100;
+    }
+      
+    getRate() {
+        return this.rateSlider.value/100; 
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-// Inititalize voice. 
+// Agent voice. 
 var agentVoice = new p5.Speech(); 
 agentVoice.onLoad = onVoicesLoaded;
 var voices = [];
 
-// Select element
-var voicesSelect; 
-var language; 
-// Rate and pitch
-var pitchSlider, rateSlider, pitchVal, rateVal;
-// User input text box; 
-var userInput; var emotion; var response; 
+// Agents. 
+var agentA; var agentB; 
+
+// User input. 
+var userInput; 
 
 // Cakechat bot
 var cakechat; 
@@ -17,63 +15,27 @@ var cakechat;
 function setup() {
   noCanvas();
   cakechat = new Cakechat(); 
+  agentA = new AgentA(); 
+  agentB = new AgentB(); 
+
+  // Voices are loaded, so set the voices for agents. 
+  agentA.setVoices(this.voices); 
+  agentB.setVoices(this.voices);
 }
 
 function draw() {
 }
 
 function onVoicesLoaded() {
-  // Initialize the system. 
-  getHTMLElements();
-
-  // Load all the voices into the drop-down menu. 
+  // Store all the voices in memory. 
   for (var v in agentVoice.voices) {
-    var option = document.createElement("option");
     var identity = {
       name: agentVoice.voices[v].name,
       language: agentVoice.voices[v].lang
     }
     voices.push(identity); 
-    option.text = identity.name; 
-    option.setAttribute('lang', identity.language);
-    voicesSelect.add(option);
   }
-
-  // Select a default value
-  voicesSelect.value = voices[0].name;
-  language.innerText = voices[0].language;
-  agentVoice.setVoice(voices[0].name)
-  
-  // Set default pitch and rate. 
-  agentVoice.setPitch(getPitch());
-  agentVoice.setRate(getRate()); 
-
-  // Set the values
-  pitchVal.innerText = getPitch(); rateVal.innerText = getRate(); 
-
   console.log('Init Voice: ' + voices[0].name);
-}
-
-function onVoiceSelected(event) {
-  var targetEl = event.target; 
-
-  // Get name and language. 
-  var idx = targetEl.selectedIndex; 
-  var name = targetEl.value; 
-  var lang = targetEl[idx].lang; 
-
-  // Set name and language. 
-  voicesSelect.value = name;
-  language.innerText = lang;
-
-  // Set the voice to this language. 
-  agentVoice.setVoice(name);
-
-  // Set default pitch and rate. 
-  agentVoice.setPitch(getPitch());
-  agentVoice.setRate(getRate());
-
-  console.log('New Voice: ' + name);
 }
 
 function onSendInput() {
@@ -81,38 +43,6 @@ function onSendInput() {
   var val = userInput.value; 
   console.log('Cakechat Request: Context ' + val + ', Emotion ' + emotion.value);
   cakechat.getResponse(val, emotion.value, this.cakechatCallback); 
-}
-
-function onPitchChange() {
-  // Set default pitch and rate. 
-  agentVoice.setPitch(getPitch());
-  pitchVal.innerText = getPitch();
-}
-
-function onRateChange() {
-  // Set default pitch and rate. 
-  agentVoice.setRate(getRate());
-  rateVal.innerText = getRate(); 
-}
-
-function getPitch() {
-  return pitchSlider.value/100;
-}
-
-function getRate() {
-  return rateSlider.value/100; 
-}
-
-function getHTMLElements() {
-  voicesSelect = document.getElementById("voices");
-  language = document.getElementById("lang");
-  pitchSlider = document.getElementById('pitch');
-  rateSlider = document.getElementById('rate'); 
-  pitchVal = document.getElementById('pitchval'); 
-  rateVal = document.getElementById('rateval');
-  userInput = document.getElementById("userinput"); 
-  emotion = document.getElementById("emotion"); 
-  response = document.getElementById("response");
 }
 
 function cakechatCallback(result, emotion) {
@@ -124,3 +54,9 @@ function cakechatCallback(result, emotion) {
     agentVoice.speak(result);
   }
 }
+
+  // agentVoice.setVoice(voices[0].name)
+  // agentVoice.setPitch(getPitch());
+  // agentVoice.setRate(getRate()); 
+
+  // // Set the values
