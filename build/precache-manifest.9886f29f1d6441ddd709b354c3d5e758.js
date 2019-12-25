3545df3fe2ac818c9ed6a51929df29696eaf3044
Amay Kataria <amkatari@microsoft.com>
2019-12-25 00:39:10 -0500

    Push build

---
+++ build/precache-manifest.9886f29f1d6441ddd709b354c3d5e758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd956863771a0f9e4dad90014b74778c",
+    "url": "/chat-feedback/index.html"
+  },
+  {
+    "revision": "fab71407ade16234cfc8",
+    "url": "/chat-feedback/static/css/main.525da7f6.chunk.css"
+  },
+  {
+    "revision": "21b32516e758d596c1a9",
+    "url": "/chat-feedback/static/js/2.dee6ba3e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/chat-feedback/static/js/2.dee6ba3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fab71407ade16234cfc8",
+    "url": "/chat-feedback/static/js/main.522f41e8.chunk.js"
+  },
+  {
+    "revision": "298e85d397cdd12a1927",
+    "url": "/chat-feedback/static/js/runtime-main.aa9613bf.js"
+  }
+]);
